484c88ed7bdcc6b80cc6c561764b50b1825fade4
j4321 <j4321@users.noreply.github.com>
2017-04-18 09:03:00 +0200

    updated setup.py
    Change package layout to be more compliant with packaging standards

--- setup.py
+++ setup.py
@@ -3,24 +3,41 @@
 
 from setuptools import setup
 
-
-files = ["images/*"]
+import os
+images = [os.path.join("mynoteslib/images/", img) for img in os.listdir("mynoteslib/images/")]
 data_files = [("share/pixmaps", ["mynotes.svg"]),
+              ("share/doc/mynotes", ["README.rst", "CHANGELOG.rst", "COPYING"]),
+              ("share/mynotes/images", images),
               ("share/locale/en_US/LC_MESSAGES/", ["mynoteslib/locale/en_US/LC_MESSAGES/MyNotes.mo"]),
               ("share/locale/fr_FR/LC_MESSAGES/", ["mynoteslib/locale/fr_FR/LC_MESSAGES/MyNotes.mo"]),
               ("share/applications", ["mynotes.desktop"])]
 
-setup(name = "mynotes",
-      version = "2.0.4",
-      description = "Post-it system tray app",
-      author = "Juliette Monsel",
-      author_email = "j_4321@protonmail.fr",
-      license = "GNU General Public License v3",
-      packages = ['mynoteslib'],
-      package_data = {'mynoteslib' : files },
-      scripts = ["mynotes"],
-      data_files = data_files,
-      long_description =
+setup(name="mynotes",
+      version="2.0.4",
+      description="Post-it system tray app",
+      keywords=["post-it", "sticky", "notes", "tkinter"],
+      author="Juliette Monsel",
+      author_email="j_4321@protonmail.com",
+      license='GPLv3',
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: X11 Applications',
+            'Intended Audience :: End Users/Desktop',
+            'Topic :: Text Editors',
+            'Topic :: Office/Business',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Natural Language :: English',
+            'Natural Language :: French',
+            'Operating System :: POSIX :: Linux',
+      ],
+      url="https://sourceforge.net/projects/my-notes",
+      packages=['mynoteslib'],
+      scripts=["mynotes"],
+      data_files=data_files,
+      long_description=
 """
 MyNotes is a sticky notes/post-it application. Notes are created using
 the system tray icon. They can be organized in categories and each
@@ -28,9 +45,8 @@ category has a color. Images, checkboxes and a few predefined symbols
 can be inserted in the notes. The style of the text can be changed
 (alignment, style).
 """,
-      requires = ["tkinter", "sys", "os", "locale", "gettext",
-                  'configparser', 'subprocess', 'time', 'shutil',
-                  'pickle', 'ewmh']
+      requires=["tkinter", "sys", "os", "locale", "gettext", 're',
+                'configparser', 'subprocess', 'time', 'shutil','html',
+                'pickle', 'ewmh']
 )
-
 
